a7d95bb2ea3b8961b58cf11638c878311f9cb1c0
link2hearth <link2hearth@gmail.com>
2025-09-28 07:47:56 +0200

    Remove sound designer studio

--- scripts/app.js
+++ scripts/app.js
@@ -1748,14 +1748,6 @@ const soundEffects = (() => {
     crit: { play: () => critPool.play(CRIT_PLAYBACK_RATE) }
   };
 
-  if (typeof window !== 'undefined' && window.SoundDesignerBridge && typeof window.SoundDesignerBridge.createSoundEffects === 'function') {
-    try {
-      return window.SoundDesignerBridge.createSoundEffects(fallbackEffects);
-    } catch (error) {
-      // If the sound designer bridge fails we still fallback to audio pools.
-    }
-  }
-
   return fallbackEffects;
 })();
 
