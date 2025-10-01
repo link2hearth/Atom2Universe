9b40109f502cd710db75fdacd91ddefa837c78e0
link2hearth <link2hearth@gmail.com>
2025-10-01 02:09:32 +0200

    Update gacha.js

--- scripts/modules/gacha.js
+++ scripts/modules/gacha.js
@@ -3,7 +3,7 @@ const DEFAULT_GACHA_TICKET_COST = 1;
 const GACHA_SMOKE_FRAME_COUNT = 91;
 const GACHA_SMOKE_FRAME_RATE = 8;
 const GACHA_SMOKE_FRAME_PAD = 4;
-const GACHA_SMOKE_ASSET_PATH = 'assets/sprites/fumée';
+const GACHA_SMOKE_ASSET_PATH = 'assets/sprites/Smoke';
 
 const gachaSmokeAnimationState = {
   timer: null,
