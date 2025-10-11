1a18a7af9a95b00dc08957ae023cf1d1cdd69904
link2hearth <link2hearth@gmail.com>
2025-10-11 18:37:40 +0200

    Configure WebView asset loading for Android

--- app/src/main/java/com/example/atom2univers/MainActivity.kt
+++ app/src/main/java/com/example/atom2univers/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.example.atom2univers
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebViewAssetLoader
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.loadUrl("file:///android_asset/index.html")
+        val webView: WebView = findViewById(R.id.webview)
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
+            .build()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest(
+                view: WebView,
+                request: WebResourceRequest
+            ): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(request.url)
+            }
+
+            override fun shouldInterceptRequest(view: WebView, url: String): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(Uri.parse(url))
+            }
+        }
+
+        with(webView.settings) {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+
+        webView.loadUrl("https://appassets.androidplatform.net/assets/index.html")
     }
 }
