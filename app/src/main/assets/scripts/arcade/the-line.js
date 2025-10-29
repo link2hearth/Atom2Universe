c2120e9aa88737e5273ecde4a508fcc8548625a3
link2hearth <link2hearth@gmail.com>
2025-10-29 09:45:48 +0100

    Fix The Line timer guard overriding method

--- app/src/main/assets/scripts/arcade/the-line.js
+++ app/src/main/assets/scripts/arcade/the-line.js
@@ -858,7 +858,7 @@
         timeoutReached = getNow() - start >= limit;
         return timeoutReached;
       },
-      get timedOut() {
+      get hasTimedOut() {
         return timeoutReached;
       }
     };
