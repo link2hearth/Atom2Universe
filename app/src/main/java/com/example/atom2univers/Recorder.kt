0bc859fc88fc8245e90f8934aca88e1e0e84b70d
link2hearth <link2hearth@gmail.com>
2025-12-12 00:12:44 +0100

    Add missing isActive import for recorder

--- app/src/main/java/com/example/atom2univers/Recorder.kt
+++ app/src/main/java/com/example/atom2univers/Recorder.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
