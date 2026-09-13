d071ba32a8841a9c8b655487653e7c4725d0e8e3
link2 <link2hearth@gmail.com>
2026-09-13 06:07:06 +0200

    edit sprites cave world

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.Atom2Universe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 5
 
         versionName = "0.1.5"
 
